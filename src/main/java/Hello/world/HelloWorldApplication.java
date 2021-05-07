a07e96e921a8da450fe3b483e5907dc291d097ea
jayaram <btjayaram666@gmail.com>
2021-05-07 14:28:16 +0530

    Hellow world batch class

--- src/main/java/Hello/world/HelloWorldApplication.java
+++ src/main/java/Hello/world/HelloWorldApplication.java
@@ -1,13 +1,56 @@
 package Hello.world;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class HelloWorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
+		System.out.println("HEllo jayaram how are you ..........");
+	}
+	@Autowired(required=true)
+	private JobBuilderFactory jobs;
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+
+	public Step step1(){
+
+		return stepBuilderFactory.get("step1").
+				tasklet(helloWorldtasklet()).
+				build();
 	}
 
+	@Bean
+	public Job job() {
+		return jobs
+				.get("taskletsJob")
+				.start(step1())
+				.build();
+	}
+	public Tasklet helloWorldtasklet(){
+
+		return (new Tasklet() {
+			@Override
+			public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("Hello world ........");
+				return RepeatStatus.FINISHED;
+			}
+		});
+
+	}
 }
